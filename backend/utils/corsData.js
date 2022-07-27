cce19abd226a86da7caffd5872c5e6ff5e3d802b
Daria Mikolaichuk <dashamikolaichuk@gmail.com>
2022-07-27 20:51:41 +0400

    fix: corsData

--- backend/utils/corsData.js
+++ backend/utils/corsData.js
@@ -3,6 +3,4 @@ module.exports.allowedCors = [
   'http://localhost:3001',
   'https://myprojectmesto.students.nomoredomains.xyz',
   'http://myprojectmesto.students.nomoredomains.xyz',
-  'http://api.myprojectmesto.students.nomored.nomoredomains.xyz',
-  'https://api.myprojectmesto.students.nomored.nomoredomains.xyz',
 ];
