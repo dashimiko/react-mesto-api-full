c5f010072f7d291b60323ae3145d2d93d6f1088f
Daria Mikolaichuk <dashamikolaichuk@gmail.com>
2022-07-27 21:18:58 +0400

    fix

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ const app = express();
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
-app.use(cors({
+app.use('*', cors({
   origin: allowedCors,
   credentials: true,
 }));
--- backend/utils/corsData.js
+++ backend/utils/corsData.js
@@ -3,4 +3,6 @@ module.exports.allowedCors = [
   'http://localhost:3001',
   'https://myprojectmesto.students.nomoredomains.xyz',
   'http://myprojectmesto.students.nomoredomains.xyz',
+  'https://api.myprojectmesto.students.nomored.nomoredomains.xyz',
+  'http://api.myprojectmesto.students.nomored.nomoredomains.xyz',
 ];
