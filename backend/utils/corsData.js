4abbf4b3c7a1360a7860838c5f2c5d692e72a44b
Daria Mikolaichuk <dashamikolaichuk@gmail.com>
2022-07-27 21:46:03 +0400

    fix

--- backend/utils/corsData.js
+++ backend/utils/corsData.js
@@ -4,8 +4,6 @@ module.exports.allowedCors = {
     'http://localhost:3001',
     'https://myprojectmesto.students.nomoredomains.xyz',
     'http://myprojectmesto.students.nomoredomains.xyz',
-    'https://api.myprojectmesto.students.nomored.nomoredomains.xyz',
-    'http://api.myprojectmesto.students.nomored.nomoredomains.xyz',
   ],
   credentials: true,
 };
